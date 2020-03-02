aeab2fa84e4d513bbdfe37857d2cbd715e28a6e1
Christopher Pilsworth <cpilsworth@gmail.com>
2020-03-02 10:27:55 +0000

    Added vpc, subnet, alb & security group

--- main.tf
+++ main.tf
@@ -2,8 +2,76 @@ provider "aws" {
   version = "~> 2.8"
   region  = "eu-west-1"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.allow_tls.id}"]
+  subnets            = ["${aws_subnet.public.*.id}"]
+
+}
+
 resource "aws_globalaccelerator_accelerator" "example" {
   name            = "apex-domain-redirect"
   ip_address_type = "IPV4"
   enabled         = true
+}
+
+resource "aws_globalaccelerator_listener" "example" {
+  accelerator_arn = aws_globalaccelerator_accelerator.example.id
+  client_affinity = "SOURCE_IP"
+  protocol        = "TCP"
+
+  port_range {
+    from_port = 443
+    to_port   = 443
+  }
+}
+
+resource "aws_globalaccelerator_endpoint_group" "example" {
+  listener_arn = aws_globalaccelerator_listener.example.id
+
+  endpoint_configuration {
+    endpoint_id = aws_lb.example.arn
+    weight      = 100
+  }
 }
