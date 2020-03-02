15c4c07ce9538aa4d0ffd65898a1971870007c3a
Christopher Pilsworth <cpilsworth@gmail.com>
2020-03-02 09:45:57 +0000

    First commit with global accellerator

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  version = "~> 2.8"
+  region = "eu-west-1"
+}
+resource "aws_globalaccelerator_accelerator" "example" {
+  name            = "apex-domain-redirect"
+  ip_address_type = "IPV4"
+  enabled         = true
+}
