d38c68f1c6dbe5f08de2f49728eec5cb107b7d24
Christopher Pilsworth <cpilsworth@gmail.com>
2020-03-02 09:52:25 +0000

    Formatted files

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version = "~> 2.8"
-  region = "eu-west-1"
+  region  = "eu-west-1"
 }
 resource "aws_globalaccelerator_accelerator" "example" {
   name            = "apex-domain-redirect"
